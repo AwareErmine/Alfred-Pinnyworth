a73c8ff6bf09b6bbd1fc11251ed67edb03d393ce
SantiagoVira <8bitharrypotter@gmail.com>
2022-01-25 23:38:47 -0500

    Fix the dumbasses' problem

--- main.js
+++ main.js
@@ -1,24 +1,46 @@
 // Require the necessary discord.js classes
-const { Client, Intents, MessageEmbed } = require("discord.js");
+const { Client, Intents, MessageEmbed, Collection } = require("discord.js");
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { token } = require("./config.json");
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
+const fs = require("fs");
+client.commands = new Collection();
+
 // When the client is ready, run this code (only once)
 client.once("ready", () => {
-	console.log("Ready!");
+    console.log("Ready!");
 });
 
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
+// Get all commands and store them
+const commandFiles = fs
+    .readdirSync("./commands/")
+    .filter((file) => file.endsWith(".js"));
 
-	const { commandName } = interaction;
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.data.name, command);
+}
 
-	if (commandName === 'test') {
-		await interaction.reply('Hiii');
-	}
+client.on("interactionCreate", async (interaction) => {
+    if (!interaction.isCommand()) return;
+
+    const command = client.commands.get(interaction.commandName);
+
+    if (!command) return;
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.log("u suck");
+        console.error(error);
+        await interaction.reply({
+            content: "There was an error while executing this command!",
+            ephemeral: true,
+        });
+    }
 });
 
 // Login to Discord with your client's token
