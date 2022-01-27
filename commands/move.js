ea26b8eca7f01fc52328ec1bca372cfdfe39c259
Aramie <aramie.ewen@gmail.com>
2022-01-27 01:25:17 -0500

    I did something probably?

--- commands/move.js
+++ commands/move.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
-const { MessageEmbed } = require('discord.js');
+const { MessageEmbed, Permissions, GuildMember } = require('discord.js');
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -8,18 +8,25 @@ module.exports = {
     .addChannelOption(option => option.setName('destination').setDescription('Select a channel')),
 
   async execute(interaction, props) {
-    // TODO: Check if the user is an admin before things
+    // Check if the user can things
+    if (!interaction.member.permissions.has(Permissions.FLAGS.MANAGE_MESSAGES)) {
+      await interaction.reply("This power is beyond thee. Begone, commoner!");
+      return;
+    }
+
+    // Check they gave a channel with the commands thing
     const destination_channel = interaction.options.getChannel('destination');
     if (destination_channel === null) {
       await interaction.reply("Pick a channel to move the pins into, silly goose!");
       return;
     };
 
-    const curr_channel = props.client.channels.cache.get(interaction.channelId);
+    const curr_channel = props.client.channels.cache.get(interaction.channelId); // channel command was sent from
     const pins = curr_channel.messages.fetchPinned().then((data) => {
       console.log(data);
       data.forEach((item, i) => {
         // I could... get embed with you ahaha
+        let messageAttachment = item.attachments.size > 0 ? [...item.attachments][0].url : null; // Check for attachments?? (broken does not)
         const pinEmbed = {
           author: {
         	   name: item.author.username,
@@ -28,6 +35,9 @@ module.exports = {
         	},
           description: item.content,
           timestamp: new Date(),
+          image: {
+            url: messageAttachment,
+          },
           footer: {
         		text: 'Forever pinned in our hearts',
         		icon_url: 'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/160/twitter/79/pushpin_1f4cc.png',
@@ -35,7 +45,7 @@ module.exports = {
         };
         destination_channel.send({ embeds: [pinEmbed] });
         // TODO: Images don't show up in the embed so that is a thing
-        // TODO: Remove the pin from the message
+        // item.unpin(); // but I could pin you ahaha (commented out for testing)
       });
     });
     // TODO: Button to confirm the move (?)
